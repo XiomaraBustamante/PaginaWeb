0b6bf9cca1d69a0380ffb44dbaeecca8d7a55e1a
aaron <aaron.morales.12lt@gmail.com>
2025-08-03 21:51:35 -0500

    cambioequipo

---
+++ jugadores.js
@@ -0,0 +1,646 @@
+
+
+  const equipo = [
+  {
+  nombre: "DIEGO ENRIQUEZ",
+  rol: "ARQUERO",
+  edad: 25,
+  altura: 1.82,
+  nacionalidad: "Peruana",
+  peso: 78.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/ENRIQUEZ.png",
+  imagenCamiseta: "images/cuerpoTecnico.png"
+},
+
+{
+  nombre: "RENATO SOLIS",
+  rol: "ARQUERO",
+  edad: 27,
+  altura: 1.88,
+  nacionalidad: "Peruana",
+  peso: 83.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/SOLIS.png",
+  imagenCamiseta: "images/camisetaArquero1.png"
+},
+
+{
+  nombre: "ALEJANDRO DUARTE",
+  rol: "ARQUERO",
+  edad: 31,
+  altura: 1.81,
+  nacionalidad: "Peruana",
+  peso: 80,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/DUARTE.png",
+  imagenCamiseta: "images/camisetaArquero1.png"
+},
+
+{
+  nombre: "CESAR BAUTISTA",
+  rol: "ARQUERO",
+  edad: 18,
+  altura: 1.96,
+  nacionalidad: "Peruana",
+  peso: 82.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/BAUTISTA.png",
+  imagenCamiseta: "images/camisetaArquero1.png"
+},
+
+{
+  nombre: "GIANFRANCO CHÁVEZ",
+  rol: "DEFENSA",
+  edad: 24,
+  altura: 1.76,
+  nacionalidad: "Peruana",
+  peso: 78,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/CHAVEZ.png",
+  imagenCamiseta: "images/camisetaJugador.png"
+},
+
+{
+  nombre: "RAFAEL LUTIGER",
+  rol: "DEFENSA",
+  edad: 24,
+  altura: 1.87,
+  nacionalidad: "Peruana",
+  peso: 80.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/LUTIGER.png",
+  imagenCamiseta: "images/camisetaJugador.png"
+},
+
+{
+  nombre: "LEANDRO SOSA",
+  rol: "DEFENSA",
+  edad: 31,
+  altura: 1.72,
+  nacionalidad: "Uruguaya",
+  peso: 69.5,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/L-SOSA.png",
+  imagenCamiseta: "images/camisetaJugador.png"
+},
+
+{
+  nombre: "JHILMAR LORA",
+  rol: "DEFENSA",
+  edad: 25,
+  altura: 1.74,
+  nacionalidad: "Peruana",
+  peso: 78,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/LORA.png",
+  imagenCamiseta: "images/camisetaJugador.png"
+},
+
+  {
+    nombre: "NICOLAS PASQUINI",
+    rol: "DEFENSA",
+    edad: 25,
+    altura: 1.74,
+    nacionalidad: "Argentina",
+    peso: 78,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/PASQUINI.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "ALEJANDRO POSITO",
+    rol: "DEFENSA",
+    edad: 20,
+    altura: 1.80,
+    nacionalidad: "Peruana",
+    peso: 81,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/POSITO.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "MIGUEL ARAUJO",
+    rol: "DEFENSA",
+    edad: 27,
+    altura: 1.82,
+    nacionalidad: "Peruana",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/ARAUJO_WEB.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "FABRICIO LORA",
+    rol: "DEFENSA",
+    edad: 20,
+    altura: 1.80,
+    nacionalidad: "Peruana",
+    peso: 68,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/Fa_LORA.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "AXEL CABELLOS",
+    rol: "DEFENSA",
+    edad: 19,
+    altura: 1.82,
+    nacionalidad: "Argentina",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/Ax_CABELLOS.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "DUHAM BALLUMBROSIO",
+    rol: "DEFENSA",
+    edad: 18,
+    altura: 1.82,
+    nacionalidad: "Peruana",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/DUHAM.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "JESUS PRETELL",
+    rol: "VOLANTE",
+    edad: 26,
+    altura: 1.72,
+    nacionalidad: "Peruana",
+    peso: 74.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/PRETELL.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "CHRISTOFER GONZALES",
+    rol: "VOLANTE",
+    edad: 32,
+    altura: 1.74,
+    nacionalidad: "Peruana",
+    peso: 71.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/C-GONZALES.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+   {
+    nombre: "YOSHIMAR YOTUN",
+    rol: "VOLANTE",
+    edad: 35,
+    altura: 1.72,
+    nacionalidad: "Peruana",
+    peso: 72.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/YOTUN.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "MAXLOREN CASTRO",
+    rol: "VOLANTE",
+    edad: 18,
+    altura: 1.68,
+    nacionalidad: "Peruana",
+    peso: 64.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/CASTRO.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "MARTIN TAVARA",
+    rol: "VOLANTE",
+    edad: 26,
+    altura: 1.79,
+    nacionalidad: "Peruana",
+    peso: 81.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/TAVARA.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "IAN WISDOM",
+    rol: "VOLANTE",
+    edad: 20,
+    altura: 1.68,
+    nacionalidad: "Peruana",
+    peso: 62,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/WISDOM.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+   {
+    nombre: "JAIR MORETTI",
+    rol: "VOLANTE",
+    edad: 18,
+    altura: 1.82,
+    nacionalidad: "Peruana",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/MORETTI23.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "GUSTAVO CAZONATTI",
+    rol: "VOLANTE",
+    edad: 25,
+    altura: 1.76,
+    nacionalidad: "Brasilera",
+    peso: 77,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/CAZONATTI.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "CATRIEL CABELLOS",
+    rol: "VOLANTE",
+    edad: 25,
+    altura: 1.72,
+    nacionalidad: "Argentina",
+    peso: 72,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/CABELLOS.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "HENRY CAPARO",
+    rol: "VOLANTE",
+    edad: 20,
+    altura: 1.72,
+    nacionalidad: "Peruana",
+    peso: 71.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/CAPARO.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+   {
+    nombre: "SANTIAGO GONZALES",
+    rol: "DELANTERO",
+    edad: 29,
+    altura: 1.70,
+    nacionalidad: "Argentina",
+    peso: 70,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/S-GONZALEZ.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "IRVEN AVILA",
+    rol: "DELANTERO",
+    edad: 35,
+    altura: 1.69,
+    nacionalidad: "Peruana",
+    peso: 76,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/AVILA.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "LUIS IBERICO",
+    rol: "DELANTERO",
+    edad: 27,
+    altura: 1.76,
+    nacionalidad: "Peruana",
+    peso: 80,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/IBERICO.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+    nombre: "MATEO RODRIGUEZ",
+    rol: "DELANTERO",
+    edad: 19,
+    altura: 1.80,
+    nacionalidad: "Peruana",
+    peso: 77,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/FOTO_RODRIGUEZ.png",
+    imagenCamiseta: "images/camisetaJugador.png"
+  },
+
+  {
+  nombre: "FERNANDO PACHECO",
+  rol: "DELANTERO",
+  edad: 26,
+  altura: 1.77,
+  nacionalidad: "Peruana",
+  peso: 81,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/PACHECO.png",
+  imagenCamiseta: "images/camisetaJugador.png"
+}];
+
+const cuerpoTecnico=[
+{
+  nombre: "PABLO AUTUORI",
+  rol: "DIRECTOR TECNICO",
+  edad: 55,
+  altura: 1.78,
+  nacionalidad: "Brasilera",
+  peso: 78.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/PROFE2.png",
+  imagenCamiseta: "images/cuerpoTecnico.png"
+},
+
+{
+  nombre: "EDSON AGUIAR",
+  rol: "ASISTENTE TECNICO",
+  edad: 38,
+  altura: 1.82,
+  nacionalidad: "Brasilera",
+  peso: 78.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/PROFE.png",
+  imagenCamiseta: "images/cuerpoTecnico.png"
+},
+
+{
+  nombre: "FABINHO EIRAS",
+  rol: "PREPARADOR FISICO",
+  edad: 24,
+  altura: 1.82,
+  nacionalidad: "Brasilera",
+  peso: 78.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/PROFE3.png",
+  imagenCamiseta: "images/cuerpoTecnico.png"
+}, 
+
+{
+  nombre: "EDERSON SANCHEZ",
+  rol: "MEDICO",
+  edad: 28,
+  altura: 1.82,
+  nacionalidad: "Brasilera",
+  peso: 78.6,
+  club: "Sporting Cristal",
+  imagenJugador: "images/Jugadores/18.png",
+  imagenCamiseta: "images/cuerpoTecnico.png"
+},
+
+ {
+    nombre: "JORGE SOTO",
+    rol: "ASISTENTE TECNICO",
+    edad: 27,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/6.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "FEDUARDO NORIEGA",
+    rol: "ASISTENTE TECNICO",
+    edad: 29,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/1.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "AUGUSTO ROSSEL",
+    rol: "PREPARADOR ARQUEROS",
+    edad: 45,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/14.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "JORDAN CCAPACCA",
+    rol: "FISIOLOGO",
+    edad: 34,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/7.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+   {
+    nombre: "ROBERTO DAVILA",
+    rol: "MEDICO",
+    edad: 28,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/roberto.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "ROSMY GAGLIUFFI",
+    rol: "FISIOTERAPEUTA",
+    edad: 27,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/3.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "DARIO CUPIDO",
+    rol: "FISIOTERAPEUTA",
+    edad: 28,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/15.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "DANIEL MONTOYA",
+    rol: "FISIOTERAPEUTA",
+    edad: 31,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/20.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "FRANCO ASCENZO",
+    rol: "PSICÓLOGO DEPORTIVO",
+    edad: 31,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/13.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "DEBORAH VIGO",
+    rol: "NUTRICIONISTA",
+    edad: 33,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/21.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "YONATAN VILCA",
+    rol: "NUTRICIONISTA",
+    edad: 35,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/19_.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "AARON TIRADO",
+    rol: "NUTRICIONISTA",
+    edad: 34,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/2.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+
+  {
+    nombre: "JOSE LUIS BOLAÑOS",
+    rol: "ANALISTA DE VIDEO",
+    edad: 45,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/16.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+  {
+    nombre: "MAXIMILIANO FRIAS",
+    rol: "JEFE DE EQUIPO",
+    edad: 34,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/17.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+  {
+    nombre: "MIGUEL LINARES",
+    rol: "JEFE DE UTILERIA",
+    edad: 28,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/24.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  },
+  {
+    nombre: "DENYS REFULIO",
+    rol: "AUXILIAR DE UTILERIA",
+    edad: 35,
+    altura: 1.82,
+    nacionalidad: "Brasilera",
+    peso: 78.6,
+    club: "Sporting Cristal",
+    imagenJugador: "images/Jugadores/23.png",
+    imagenCamiseta: "images/cuerpoTecnico.png"
+  }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    //obtener los id del jugador y cuerportecnico
+  const contenedorJugadores = document.getElementById('contenedorJugadores');
+  const contenedorCuerpoTecnico = document.getElementById('contenedorCuerpoTecnico');
+
+  // Mostrar jugadores, recorre el arreglo con for each
+  //jugador es una variable que representa a cada elemento del arreglo
+  //con la funcion creo la carta de un jugador y al final la muestro con append
+  equipo.forEach(jugador => {
+    const carta = crearCarta(jugador);
+    contenedorJugadores.appendChild(carta);
+  });
+
+  // Mostrar cuerpo técnico
+  cuerpoTecnico.forEach(persona => {
+    const carta = crearCarta(persona);
+    contenedorCuerpoTecnico.appendChild(carta);
+  });
+
+  //Accion para que se volten las cartas
+  //primjero seleccionamos todas las cartas que tengan la clase .contenedorCarta
+  //cuando le debemos clik toggle hace que gire su posicion 
+  const cartas = document.querySelectorAll('.contenedorCarta');
+  cartas.forEach(carta => {
+    carta.addEventListener('click', () => {
+      carta.classList.toggle('flipped');
+    });
+  });
+});
+
+// Crear una carta, a la funcion crearcarta le paso como parametro un nombre que represente a persona y jugador
+function crearCarta(persona) {
+  const carta = document.createElement('div');
+  carta.className = 'contenedorCarta';
+  carta.innerHTML = `
+    <div class="carasUnidas">
+      <div class="caraFrontal">
+        <img src="${persona.imagenJugador}" alt="${persona.nombre}">
+        <h3>${persona.nombre}</h3>
+        <p>${persona.rol}</p>
+      </div>
+      <div class="caraTrasera">
+        <h3 class="nombreJugador">${persona.nombre}</h3>
+        <ul class="datosJugador">
+          <li><strong>🎯 Edad:</strong> ${persona.edad} años</li>
+          <li><strong>📏 Altura:</strong> ${persona.altura} m</li>
+          <li><strong>🌎 Nacionalidad:</strong> ${persona.nacionalidad}</li>
+          <li><strong>⚖️ Peso:</strong> ${persona.peso} kg</li>
+          <li><strong>🏟️ Club:</strong> ${persona.club}</li>
+        </ul>
+        <img src="${persona.imagenCamiseta}" alt="Camiseta">
+      </div>
+    </div>
+  `;
+  return carta;
+}
